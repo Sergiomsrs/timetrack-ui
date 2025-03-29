bf883829bd59444a3ce944e1960916fde06908f5
Sergio <sergiomsrs@gmail.com>
2025-03-29 10:54:01 +0100

    cambio logo header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Logo" />
+            <img className="h-8 w-auto" src="src\assets\logo.png" alt="Logo" />
           </a>
         </div>
 
