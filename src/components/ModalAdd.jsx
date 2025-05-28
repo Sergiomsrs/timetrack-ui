39ce15fa7ef4ce5d2d18f1898ec1561e17f1eea8
Sergio <sergiomsrs@gmail.com>
2025-05-28 07:35:03 +0200

    fix: actualizar el texto del footer

--- src/components/ModalAdd.jsx
+++ src/components/ModalAdd.jsx
@@ -133,7 +133,7 @@ export const ModalAdd = ({ setIsModalAddOpen, selectedEmployeeId, employees, set
                 <input
                   type="date"
                   onChange={handleDateChange}
-                  className="border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-violet-500"
                   />
               </div>
 
@@ -142,7 +142,7 @@ export const ModalAdd = ({ setIsModalAddOpen, selectedEmployeeId, employees, set
                 <input
                   type="time"
                   onChange={handleTimeChange}
-                  className="border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-violet-500"
                   />
               </div>
                
